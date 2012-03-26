25a19381b85fa303e6e89a9b179c3c92a4aa102a
Arthur Wolf <wolf.arthur@gmail.com>
2012-03-26 19:35:07 +0200

    forgot StreamOutput

---
+++ src/libs/StreamOutput.h
@@ -0,0 +1,22 @@
+#ifndef STREAMOUTPUT_H
+#define STREAMOUTPUT_H
+
+class StreamOutput {
+    public:
+       StreamOutput(){}
+       int printf(const char* format, ...){}
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif
