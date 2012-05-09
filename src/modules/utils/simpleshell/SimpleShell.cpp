8bcf74dcf9d9cf9d57478af98cfcf1ba803c6650
Logxen <logxen@hotmail.com>
2012-05-09 04:38:46 -0700

    made SimpleShell wait for the queue to have room so that a serial message can still be received while playing a file

--- src/modules/utils/simpleshell/SimpleShell.cpp
+++ src/modules/utils/simpleshell/SimpleShell.cpp
@@ -117,6 +117,8 @@ void SimpleShell::on_main_loop(void* argument){
                 struct SerialMessage message; 
                 message.message = buffer;
                 message.stream = this->current_stream;
+                // wait for the queue to have enough room that a serial message could still be received before sending
+                while(this->kernel->player->queue.size() >= this->kernel->player->queue.capacity() - 2) { wait_us(500); }
                 this->kernel->call_event(ON_CONSOLE_LINE_RECEIVED, &message); 
                 buffer.clear();
                 return;
