2b3944364e3dcf68bf7c6f9ae1b434e00136b165
Logxen <logxen@hotmail.com>
2012-06-19 11:42:06 -0700

    cleaning of Configurator for new Smoothified config system

--- src/modules/utils/configurator/Configurator.cpp
+++ src/modules/utils/configurator/Configurator.cpp
@@ -15,8 +15,8 @@
 
 
 void Configurator::on_module_loaded(){
-//    this->register_for_event(ON_CONSOLE_LINE_RECEIVED);
-    this->register_for_event(ON_GCODE_EXECUTE);
+    this->register_for_event(ON_CONSOLE_LINE_RECEIVED);
+//    this->register_for_event(ON_GCODE_EXECUTE);
 //    this->register_for_event(ON_MAIN_LOOP);
 }
 
@@ -26,7 +26,7 @@ void Configurator::on_console_line_received( void* argument ){
     string possible_command = new_message.message;
 
     // We don't compare to a string but to a checksum of that string, this saves some space in flash memory
-    unsigned short check_sum = get_checksum( possible_command.substr(0,possible_command.find_first_of(" \r\n")) );  // todo: put this method somewhere more convenient
+    uint16_t check_sum = get_checksum( possible_command.substr(0,possible_command.find_first_of(" \r\n")) );  // todo: put this method somewhere more convenient
 
     // Act depending on command
     switch( check_sum ){
@@ -50,25 +50,48 @@ void Configurator::on_gcode_execute(void* argument){
     }
 }
 
-void Configurator::on_main_loop(void* argument){
+void Configurator::on_main_loop(void* argument){}
+
+// Output a ConfigValue from the specified ConfigSource to the stream
+void Configurator::config_get_command( string parameters, StreamOutput* stream ){
+    uint16_t source_checksum = get_checksum( shift_parameter(parameters) );
+    uint16_t setting_checksum = get_checksum( shift_parameter(parameters) );
+    if (setting_checksum == 0) { // output live setting
+        setting_checksum = source_checksum;
+        source_checksum = 0;
+        stream.printf( this->kernel->config->value(setting_checksum)->as_string().c_str() );
+    } else { // output setting from specified source
+        stream.printf( "WARNING: Reading from a specific source is unimplemented\r\n" );
+    }
 }
 
-void Configurator::config_get_command( string parameters, StreamOutput* stream ){}
-void Configurator::config_set_command( string parameters, StreamOutput* stream ){}
+// Write the specified setting to the specified ConfigSource
+void Configurator::config_set_command( string parameters, StreamOutput* stream ){
+    string source = shift_parameter(parameters);
+    string setting = shift_parameter(parameters);
+    string value = shift_parameter(parameters);
+    if (value == "") {
+        value = setting;
+        setting = source;
+        source = "";
+        stream.printf( "WARNING: Writing to live values is unimplemented\r\n" );
+    } else {
+        uint16_t source_checksum = get_checksum(source);
+        uint16_t setting_checksum = get_checksum(setting);
+    }
+}
+
+// Reload config values from the specified ConfigSource
 void Configurator::config_load_command( string parameters, StreamOutput* stream ){}
 
 // Write live settings to internal storage
-void Configurator::config_store( Gcode* gcode, StreamOutput* stream ){
-}
+void Configurator::config_store( Gcode* gcode, StreamOutput* stream ){}
 
 // Read settings from internal storage
-void Configurator::config_read( Gcode* gcode, StreamOutput* stream ){
-}
+void Configurator::config_read( Gcode* gcode, StreamOutput* stream ){}
 
 // Reset live settings to defaults
-void Configurator::config_defaults( Gcode* gcode, StreamOutput* stream ){
-}
+void Configurator::config_defaults( Gcode* gcode, StreamOutput* stream ){}
 
 // Output the current live settings to the stream
-void Configurator::config_print( Gcode* gcode, StreamOutput* stream ){
-}
+void Configurator::config_print( Gcode* gcode, StreamOutput* stream ){}
