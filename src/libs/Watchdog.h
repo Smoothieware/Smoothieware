6eeb045af42f0f15cb7bee164b5113194ac288a5
Michael Moon <triffid.hunter@gmail.com>
2013-01-23 13:42:49 +1100

    use MRI hooks to disable heaters in debug mode

--- src/libs/Watchdog.h
+++ src/libs/Watchdog.h
@@ -3,17 +3,27 @@
 
 #include <stdint.h>
 
+#include "Module.h"
+
 typedef enum
 {
     WDT_MRI,
     WDT_RESET,
 } WDT_ACTION;
 
-class Watchdog
+extern "C" {
+    void __mriPlatform_EnteringDebuggerHook();
+    void __mriPlatform_LeavingDebuggerHook();
+}
+
+class Watchdog : public Module
 {
 public:
     Watchdog(uint32_t timeout, WDT_ACTION action);
     void feed();
+
+    void on_module_loaded();
+    void on_idle(void*);
 };
 
 #endif /* _WATCHDOG_H */
