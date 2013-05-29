f91e7c58f9df33b1f9ced69d9841f7ce7671881f
Jim Morris <morris@wolfman.com>
2013-05-29 04:34:26 -0700

    add placeholder for git info

--- src/version.cpp
+++ src/version.cpp
@@ -1,6 +1,6 @@
 #include "version.h"
 const char *Version::get_build(void) const {
-    return "feature/add-version-command-582559c";
+    return "placeholder";
 }
 const char *Version::get_build_date(void) const {
     return __DATE__ " " __TIME__;
