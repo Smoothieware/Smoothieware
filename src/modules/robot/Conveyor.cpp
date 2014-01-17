8698e81a100c0d41d97c17a969191c4c8ec8a566
Michael Moon <triffid.hunter@gmail.com>
2014-01-17 22:23:39 +1100

    Conveyor: add verbose commentary

--- src/modules/robot/Conveyor.cpp
+++ src/modules/robot/Conveyor.cpp
@@ -21,7 +21,32 @@ using namespace std;
 
 #define planner_queue_size_checksum CHECKSUM("planner_queue_size")
 
-// The conveyor holds the queue of blocks, takes care of creating them, and starting the executing chain of blocks
+/*
+ * The conveyor holds the queue of blocks, takes care of creating them, and starting the executing chain of blocks
+ *
+ * The Queue is implemented as a ringbuffer- with a twist
+ *
+ * Since delete() is not thread-safe, we must marshall deletable items out of ISR context
+ *
+ * To do this, we have implmented a *double* ringbuffer- two ringbuffers sharing the same ring, and one index pointer
+ *
+ * as in regular ringbuffers, HEAD always points to a clean, free block. We are free to prepare it as we see fit, at our leisure.
+ * When the block is fully prepared, we increment the head pointer, and from that point we must not touch it anymore.
+ *
+ * also, as in regular ringbuffers, we can 'use' the TAIL block, and increment tail pointer when we're finished with it
+ *
+ * Both of these are implemented here- see queue_head_block() (where head is pushed) and on_idle() (where tail is consumed)
+ *
+ * The double ring is implemented by adding a third index pointer that lives in between head and tail. We call it gc_pending which describes its function rather than its operation
+ *
+ * in ISR context, we use HEAD as the head pointer, and gc_pending as the tail pointer.
+ * As HEAD increments, ISR context can consume the new blocks which appear, and when we're finished with a block, we increment gc_pending to signal that they're finishd, and ready to be cleaned
+ *
+ * in IDLE context, we use gc_pending as the head pointer, and TAIL as the tail pointer.
+ * When gc_pending != tail, we clean up the tail block (performing ISR-unsafe delete operations) and consume it (increment tail pointer), returning it to the pool of clean, unused blocks which HEAD is allowed to prepare for queueing
+ *
+ * Thus, our two ringbuffers exist sharing the one ring of blocks, and we safely marshall used blocks from ISR context to IDLE context for safe cleanup.
+ */
 
 Conveyor::Conveyor(){
     gc_pending = queue.tail_i;
@@ -37,6 +62,7 @@ void Conveyor::on_module_loaded(){
 }
 
 // Delete blocks here, because they can't be deleted in interrupt context ( see Block.cpp:release )
+// note that blocks get cleaned as they come off the tail, so head ALWAYS points to a cleaned block.
 void Conveyor::on_idle(void* argument){
     if (queue.tail_i != gc_pending)
     {
@@ -52,6 +78,24 @@ void Conveyor::on_idle(void* argument){
         }
     }
 }
+
+/*
+ * In on_main_loop, we check whether the queue should be running, but isn't.
+ *
+ * The main trigger for this event is other pieces of code adding gcode to a block, but not pushing it. This occurs frequently with gcodes that must be executed at the correct point in the queue, but take zero time to execute.
+ * Smoothie will happily attach many of such gcodes onto a single block, to save room in the queue.
+ *
+ * Any gcode which can potentially take time to execute, or might like to halt the queue MUST push the head block, otherwise gcodes that arrive later may get executed at the same time, and gcode execution order strictness would be violated.
+ *
+ * If we get back to main loop context and the block has gcode but isn't pushed, then we can safely push it and start the queue.
+ *
+ *
+ * It's also theoretically possible that a race condition could occur where we pop the final block and stop the queue, while at the same time main loop is pushing head but thinks the queue is running and thus does not start it.
+ *
+ * In this case, we start the queue again when execution returns to main loop.
+ * No stuttering or other visible effects could be caused by this event, as the planner will have set the last block to decelerate to zero, and the new block to accelerate from zero.
+ *
+ */
 
 void Conveyor::on_main_loop(void*)
 {
@@ -129,6 +173,9 @@ bool Conveyor::is_queue_empty()
     return queue.is_empty();
 }
 
+/*
+ * push the pre-prepared head block onto the queue
+ */
 void Conveyor::queue_head_block()
 {
     while (queue.is_full())
