e2a640c0be6402f3eccdb9d5a3b470130b24118c
Michael Moon <triffid.hunter@gmail.com>
2014-01-19 17:24:27 +1100

    Found an error state which doesn't cause linux to spam usb resets

--- src/libs/USBDevice/USBMSD/USBMSD.cpp
+++ src/libs/USBDevice/USBMSD/USBMSD.cpp
@@ -371,11 +371,15 @@ void USBMSD::memoryVerify (uint8_t * buf, uint16_t size) {
 
 bool USBMSD::inquiryRequest (void) {
     uint8_t inquiry[] = { 0x00, 0x80, 0x00, 0x01,
-                          36 - 4, 0x80, 0x00, 0x00,
+                          36 - 4, 0x00, 0x00, 0x01,
                           'M', 'B', 'E', 'D', '.', 'O', 'R', 'G',
                           'M', 'B', 'E', 'D', ' ', 'U', 'S', 'B', ' ', 'D', 'I', 'S', 'K', ' ', ' ', ' ',
                           '1', '.', '0', ' ',
                         };
+
+    if (BlockCount == 0)
+        inquiry[0] = 0x20; // PERIPHERAL_QUALIFIER = 1 : "A peripheral device is not connected, however usually we do support this type of peripheral"
+
     if (!write(inquiry, sizeof(inquiry))) {
         return false;
     }
@@ -483,9 +487,9 @@ bool USBMSD::requestSense (void) {
 
     if (BlockCount == 0)
     {
-        request_sense[2] = 0x02; // Not Ready
-        request_sense[3] = 0x3A; // medium not present
-        request_sense[4] = 0x00; // medium not present - no specific reason
+        request_sense[ 2] = 0x02; // Not Ready
+        request_sense[12] = 0x3A; // Medium not present
+        request_sense[13] = 0x00; // No known reason
     }
 
     if (!write(request_sense, sizeof(request_sense))) {
@@ -623,7 +627,8 @@ void USBMSD::testUnitReady (void) {
     if (BlockCount > 0)
         csw.Status = CSW_PASSED;
     else
-        csw.Status = CSW_FAILED;
+        csw.Status = CSW_ERROR;
+
     sendCSW();
 }
 
