6dff28aa121486be10d389c0010246c2cba32112
Logxen <logxen@hotmail.com>
2014-05-17 15:40:09 -0700

    M105 works for all controllers again

--- src/modules/tools/temperaturecontrol/TemperatureControl.cpp
+++ src/modules/tools/temperaturecontrol/TemperatureControl.cpp
@@ -181,7 +181,7 @@ void TemperatureControl::on_gcode_received(void* argument){
                 this->active = toolmanager.current_tool_name == this->name_checksum;
             }
         }
-        if( (gcode->m == this->get_m_code) && this->active ){
+        if( gcode->m == this->get_m_code ){
             char buf[32]; // should be big enough for any status
             int n= snprintf(buf, sizeof(buf), "%s:%3.1f /%3.1f @%d ", this->designator.c_str(), this->get_temperature(), ((target_temperature == UNDEFINED)?0.0:target_temperature), this->o);
             gcode->txt_after_ok.append(buf, n);
