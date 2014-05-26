13db8ee6e52ea2ce3ae4efd1eb69fd656378ffa3
0x23 <manuel.scholz@web.de>
2014-05-26 13:50:45 +0200

    added a lightweight least squares optimizer
    Levenberg-Marquard optimizer for nonlinear least squares problems
    Can be useful for calibration of kinematic models

---
+++ src/libs/Optimization.h
@@ -0,0 +1,26 @@
+// author: manuel scholz
+
+#ifndef LEASTSQUARES_OPTIMIZATION_H
+#define LEASTSQUARES_OPTIMIZATION_H
+
+//*** CLASS ***************************************************************************************
+
+class CLeastSquareProblem {
+  public:
+    // optimizes the least squares problem with the levenberg-marquard optimization method
+    float	  optimizeLevMar(float* Parameter, int MaxIterations, float MaxError = 0.0);
+
+    // returns number of system parameter
+    virtual int	  getParameterCount() = 0;
+    // returns the size of the residual vector (often the number of measurements)
+    virtual int	  getResidualElementCount() = 0;
+    // computes the residual vector (e.g. differences between model and measurements). The entries must not be squared ! e.g. store distances not squared distances )
+    virtual void  computeResidualVector(float* ParameterVector, float* ResidualVector) = 0;
+    // called after each iteration, parameter constraints can be enforced here. also useful for user feedback
+    virtual void  onIterationFinished(int Itertaion, float RMSError) {};
+
+  protected:
+    void computeResidualJacobiMatrix(float* ParameterVector, float h, float* mJacobiMatrix, float* Residual);
+};
+
+#endif
