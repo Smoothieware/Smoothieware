78308d5f1ddc93e9ff555afbe000b07d30bfed97
0x23 <manuel.scholz@web.de>
2014-05-26 14:24:45 +0200

    Update Optimization.h

--- src/libs/math/Optimization.h
+++ src/libs/math/Optimization.h
@@ -6,21 +6,21 @@
 //*** CLASS ***************************************************************************************
 
 class CLeastSquareProblem {
-  public:
-    // optimizes the least squares problem with the levenberg-marquard optimization method
-    float         optimizeLevMar(float* Parameter, int MaxIterations, float MaxError = 0.0);
+    public:
+        // optimizes the least squares problem with the levenberg-marquard optimization method
+        float         optimizeLevMar(float* Parameter, int MaxIterations, float MaxError = 0.0);
 
-    // returns number of system parameter
-    virtual int	  getParameterCount() = 0;
-    // returns the size of the residual vector (often the number of measurements)
-    virtual int	  getResidualElementCount() = 0;
-    // computes the residual vector (e.g. differences between model and measurements). The entries must not be squared ! e.g. store distances not squared distances )
-    virtual void  computeResidualVector(float* ParameterVector, float* ResidualVector) = 0;
-    // called after each iteration, parameter constraints can be enforced here. also useful for user feedback
-    virtual void  onIterationFinished(int Itertaion, float RMSError) {};
+        // returns number of system parameter
+        virtual int	  getParameterCount() = 0;
+        // returns the size of the residual vector (often the number of measurements)
+        virtual int	  getResidualElementCount() = 0;
+        // computes the residual vector (e.g. differences between model and measurements). The entries must not be squared ! e.g. store distances not squared distances )
+        virtual void  computeResidualVector(float* ParameterVector, float* ResidualVector) = 0;
+        // called after each iteration, parameter constraints can be enforced here. also useful for user feedback
+        virtual void  onIterationFinished(int Itertaion, float RMSError) {};
 
-  protected:
-    void computeResidualJacobiMatrix(float* ParameterVector, float h, float* mJacobiMatrix, float* Residual);
+      protected:
+        void computeResidualJacobiMatrix(float* ParameterVector, float h, float* mJacobiMatrix, float* Residual);
 };
 
 #endif
