b0fc8a530db070621d10076f10754b7ccdd14366
Jim Morris <morris@wolfman.com>
2014-07-24 15:39:04 -0700

    cleanup include files

--- src/modules/utils/currentcontrol/CurrentControl.cpp
+++ src/modules/utils/currentcontrol/CurrentControl.cpp
@@ -1,5 +1,5 @@
-#include "libs/Kernel.h"
 #include "CurrentControl.h"
+#include "libs/Kernel.h"
 #include "libs/nuts_bolts.h"
 #include "libs/utils.h"
 #include "ConfigValue.h"
@@ -8,6 +8,7 @@
 #include "Gcode.h"
 #include "Config.h"
 #include "checksumm.h"
+#include "DigipotBase.h"
 
 // add new digipot chips here
 #include "mcp4451.h"
@@ -15,6 +16,22 @@
 
 #include <string>
 using namespace std;
+
+#define alpha_current_checksum                  CHECKSUM("alpha_current")
+#define beta_current_checksum                   CHECKSUM("beta_current")
+#define gamma_current_checksum                  CHECKSUM("gamma_current")
+#define delta_current_checksum                  CHECKSUM("delta_current")
+#define epsilon_current_checksum                CHECKSUM("epsilon_current")
+#define zeta_current_checksum                   CHECKSUM("zeta_current")
+#define eta_current_checksum                    CHECKSUM("eta_current")
+#define theta_current_checksum                  CHECKSUM("theta_current")
+#define currentcontrol_module_enable_checksum   CHECKSUM("currentcontrol_module_enable")
+#define digipotchip_checksum                    CHECKSUM("digipotchip")
+#define digipot_max_current                     CHECKSUM("digipot_max_current")
+#define digipot_factor                          CHECKSUM("digipot_factor")
+
+#define mcp4451_checksum                        CHECKSUM("mcp4451")
+#define ad5206_checksum                         CHECKSUM("ad5206")
 
 CurrentControl::CurrentControl()
 {
