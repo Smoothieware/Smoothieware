abf5ca2060574ff9f7277743dd07bf15e99a4271
hogthrob <hogthrob@github.com>
2014-08-25 10:58:59 +0200

    Approach to generate branch name for version string now identical to linux/macos_x approach

--- src/generate-version.bat
+++ src/generate-version.bat
@@ -1,5 +1,5 @@
-@for /f "tokens=3* delims=/" %%a in ('git symbolic-ref HEAD') do @set myvar=%%a
+@for /f "delims=" %%a in ('git symbolic-ref HEAD') do @set myvar=%%a
 @for /f "delims=" %%b in ('git log "--pretty=format:%%h" -1') do @set myvar2=%%b
-@echo %myvar%-%myvar2%
+@echo %myvar:~11%-%myvar2%
 @copy /b  version.cpp+,, >NUL:
 
