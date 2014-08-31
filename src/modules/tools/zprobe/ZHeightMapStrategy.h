8f199cacf3fd5adc0240135ae9127c7586da49b7
Quentin Harley <quentin.harley@gmail.com>
2014-08-31 15:40:07 +0200

    Initial implementation.  Non functional

--- src/modules/tools/zprobe/ZHeightMapStrategy.h
+++ src/modules/tools/zprobe/ZHeightMapStrategy.h
@@ -1,15 +1,15 @@
-#ifndef _THREEPOINTSTRATEGY
-#define _THREEPOINTSTRATEGY
+#ifndef _ZHEIGHTMAPSTRATEGY
+#define _ZHEIGHTMAPSTRATEGY
 
 #include "LevelingStrategy.h"
 
 #include <string.h>
-#include <tuple>
+#include <stdint.h>
 
-#define three_point_leveling_strategy_checksum CHECKSUM("three-point-leveling")
+#define zheightmap_leveling_checksum CHECKSUM("zheightmap-leveling")
 
 class StreamOutput;
-class Plane3D;
+//class Plane3D;
 
 typedef struct
 {
@@ -18,34 +18,39 @@ typedef struct
     float *pData;
 } arm_bilinear_interp_instance;
 
-class GridStrategy : public LevelingStrategy
+class ZHeightMapStrategy : public LevelingStrategy
 {
 public:
-    GridStrategy(ZProbe *zprobe);
-    ~GridStrategy();
+    ZHeightMapStrategy(ZProbe *zprobe);
+    ~ZHeightMapStrategy();
     bool handleGcode(Gcode* gcode);
     bool handleConfig();
     float getZOffset(float x, float y);
 
     arm_bilinear_interp_instance bed_level_data;
 
-
-
-
 private:
-    void home();
+    void homexyz();
 
     void move(float *position, float feed);
     void next_cal(void);
 
+    float arm_bilinear_interp(float X, float Y);
+
+    void setAdjustFunction(bool);
+
     float slow_rate;
+    float bed_x;
+    float bed_y;
+    float bed_div_x;
+    float bed_div_y;
     float cal[3];            // calibration positions for manual leveling: TODO - remove when auto is functional
-    bool in_cal;
 
     struct {
         bool home:1;
         bool save:1;
         bool is_scara:1;
+        bool in_cal:1;
     };
 };
 
