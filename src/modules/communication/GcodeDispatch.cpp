08d9afc6245fe7e3dbe8e3e377cbd9e699f77f20
Jim Morris <morris@wolfman.com>
2014-09-15 16:05:18 -0700

    add crnl to end of M503 to flush in some cases use puts in usbserial to output the banner

--- src/modules/communication/GcodeDispatch.cpp
+++ src/modules/communication/GcodeDispatch.cpp
@@ -166,6 +166,7 @@ try_again:
                                 } else {
                                     new_message.stream->printf("; No config override\n");
                                 }
+                                gcode->add_nl= true;
                                 break; // fall through to process by modules
                             }
                         }
