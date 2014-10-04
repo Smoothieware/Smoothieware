ad71624f6a541aa0dcb71b385c43f42c670b2b8f
Quentin Harley <quentin.harley@gmail.com>
2014-10-04 13:05:22 +0200

    First working zheightmap strategy for ZProbe will rpbe 25 points on the pronting surface, create a surface map, and store in "grid" on the SD card.
    It uses bilinear interpolation to have Z follow the bed surface throughout
    printing the object, and is critical on oversized and axial robot systems.
    
    The module assumes a squared printing surface as it does not change the
    plane of the printing surface, but will allow printing on a slightly skew bed.

--- src/modules/tools/zprobe/ZProbe.cpp
+++ src/modules/tools/zprobe/ZProbe.cpp
@@ -29,6 +29,7 @@
 // strategies we know about
 #include "DeltaCalibrationStrategy.h"
 #include "ThreePointStrategy.h"
+#include "ZHeightMapStrategy.h"
 
 #define enable_checksum          CHECKSUM("enable")
 #define probe_pin_checksum       CHECKSUM("probe_pin")
@@ -92,6 +93,11 @@ void ZProbe::on_config_reload(void *argument)
                     this->strategies.push_back(new ThreePointStrategy(this));
                     found= true;
                     break;
+
+                case zheightmap_leveling_checksum:
+                     this->strategies.push_back(new ZHeightMapStrategy(this));
+                     found= true;
+                     break;
 
                 // add other strategies here
                 //case zheight_map_strategy:
