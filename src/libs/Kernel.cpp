e4060720d539c017b5b8713e316a075dc72a93cf
Clément Léger <clement.leger@kalray.eu>
2015-04-12 23:40:39 +0200

    Kernel: remove empty newline

--- src/libs/Kernel.cpp
+++ src/libs/Kernel.cpp
@@ -46,7 +46,6 @@ Kernel::Kernel(){
 	// Set to UART0, this will be changed to use the same UART as MRI if it's enabled
     this->serial = new SerialConsole(USBTX, USBRX, DEFAULT_SERIAL_BAUD_RATE);
 
-
     // Config next, but does not load cache yet
     this->config = new Config();
 
