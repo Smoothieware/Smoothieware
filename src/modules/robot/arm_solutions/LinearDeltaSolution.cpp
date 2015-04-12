10e38fd92060531786b77a1a284ccb8d36eb5bde
626Pilot <doot@startingstate.com>
2015-04-11 18:26:00 -0700

    Added partial support for tower lean (turned off at the moment as it isn't ready yet)

--- src/modules/robot/arm_solutions/LinearDeltaSolution.cpp
+++ src/modules/robot/arm_solutions/LinearDeltaSolution.cpp
@@ -36,6 +36,11 @@ LinearDeltaSolution::LinearDeltaSolution(Config* config)
     tower1_offset = config->value(tower1_offset_checksum)->by_default(0.0f)->as_number();
     tower2_offset = config->value(tower2_offset_checksum)->by_default(0.0f)->as_number();
     tower3_offset = config->value(tower3_offset_checksum)->by_default(0.0f)->as_number();
+    
+    float up_vector[3] = { 0, 0, 1 };
+    std::memcpy(tower1_vector, up_vector, sizeof(tower1_vector));
+    std::memcpy(tower2_vector, up_vector, sizeof(tower2_vector));
+    std::memcpy(tower3_vector, up_vector, sizeof(tower3_vector));
 
     init();
 }
@@ -58,6 +63,76 @@ void LinearDeltaSolution::init() {
 void LinearDeltaSolution::cartesian_to_actuator( float cartesian_mm[], float actuator_mm[] )
 {
 
+    // Here, we answer the following question:
+    // If we want the effector to be somewhere, how far does each carriage need to be from the effector?
+
+    /*
+
+        Math of doing it the old way, which assumes that all towers are aligned with the vector { 0, 0, 1 }:
+
+        For simplicity's sake, the effector is resting on the print surface. Elevations other than Z=0 are
+        achieved by adding Z to all three carriage positions.
+
+                          .<--- Carriage
+        Carriage Z      T |\
+        = Height of     o | \
+          carriage      w |  \  AL = Arm Length
+          above build   e |   \
+          surface       r |    \
+                          .-----.<--- Effector
+                           Dist
+
+          CarZ = sqrt(AL^2 + Dist^2)
+            AL = sqrt(Dist^2 + CarZ^2)
+          Dist = sqrt(CarZ^2 + AL^2)
+       
+        Dist^2 = (X - twr[X])^2 + (Y - twr[Y])^2
+               = AL^2 - CarZ^2
+       
+          CarZ = sqrt( AL^2 - (pos[X]-twr[X])^2 - (pos[Y]-twr[Y])^2 ) + Z
+
+        New way requires that we treat the towers as being at some arbitrary rotation, rather than {0, 0, 1}. The
+        variables twr[X] and twr[Y] must be reckoned according to effector axis distance from zero. Because a tilted arm
+        has a vector where the .Z component can be != 1.0, not just X and Y, but also Z, are effected.
+
+        Actuator distance-from-zero to carriage XYZ:
+            XYZ = { tower[X|Y|Z] + (actuator_mm * tower_vector[X|Y|Z]), same, same }
+
+        The above equation - CarZ = sqrt(AL^2 - (posX-twrX)^2 - (posY-twrY)^2) + Z - will not work in this configuration
+        because the tower's X and Y can't be known before its Z (which the equation is supposed to find!) is also known.
+
+        For one thing, we are now dealing with a SCALENE rather than a right triangle. This is because the "upright" leg
+        may be at an angle with the "Dist" leg that is NOT 90 degrees! The 2D right-triangle problem is now a 3D scalene
+        triangle problem.
+
+        Find CarZ according to the normal equation:
+
+            CarZ = sqrt( AL^2 - (pos[X]-twr[X])^2 - (pos[Y]-twr[Y])^2 )
+
+        Then, find the carriage coords using our new algorithm:
+
+            XYZ = { tower[X|Y|Z] + (CarZ * tower_vector[X|Y|Z]), same, same }
+            CarZ = XYZ[Z]
+
+        What is the difference between these two?
+    
+        In any triangle (scalene or otherwise), side / sin(angle opposite that side) is a ratio that is the same for
+        all sides of the triangle. For example, in a triangle with sides ABC and angles opposite those sides abc,
+        A/sin(A) == B/sin(b) == C/sin(c).
+        
+        
+
+
+
+
+
+
+    */
+
+
+
+
+
     actuator_mm[ALPHA_STEPPER] = sqrtf(this->arm_length_squared
                                        - SQ(delta_tower1_x - cartesian_mm[X_AXIS])
                                        - SQ(delta_tower1_y - cartesian_mm[Y_AXIS])
@@ -76,14 +151,35 @@ void LinearDeltaSolution::cartesian_to_actuator( float cartesian_mm[], float act
 // At the time of writing, nothing appears to call this method for any reason except ComprehensiveDeltaStrategy (see ZProbe module)
 void LinearDeltaSolution::actuator_to_cartesian( float actuator_mm[], float cartesian_mm[] )
 {
+
+    // Here, we answer the following question:
+    // If the actuators are in the given positions, where will the effector be?
+
     // from http://en.wikipedia.org/wiki/Circumscribed_circle#Barycentric_coordinates_from_cross-_and_dot-products
     // based on https://github.com/ambrop72/aprinter/blob/2de69a/aprinter/printer/DeltaTransform.h#L81
     
     // Current tower locations & carriage (actuator) positions
+    // Old code:
     Vector3 tower1( delta_tower1_x, delta_tower1_y, actuator_mm[0] );
     Vector3 tower2( delta_tower2_x, delta_tower2_y, actuator_mm[1] );
     Vector3 tower3( delta_tower3_x, delta_tower3_y, actuator_mm[2] );
 
+    // New code:
+/*
+    float xyz[3];
+    
+    // X tower
+    get_tower_xyz_for_dist(X, xyz, actuator_mm[X]);
+    Vector3 tower1( xyz[X], xyz[Y], xyz[Z] );
+
+    // Y tower
+    get_tower_xyz_for_dist(Y, xyz, actuator_mm[Y]);
+    Vector3 tower2( xyz[X], xyz[Y], xyz[Z] );
+
+    // Z tower
+    get_tower_xyz_for_dist(Z, xyz, actuator_mm[Z]);
+    Vector3 tower3( xyz[X], xyz[Y], xyz[Z] );
+*/
     // Median points between carriages
     Vector3 s12 = tower1.sub(tower2);
     Vector3 s23 = tower2.sub(tower3);
@@ -122,6 +218,48 @@ void LinearDeltaSolution::actuator_to_cartesian( float actuator_mm[], float cart
     cartesian_mm[2] = ROUND(cartesian[2], 4);
 }
 
+// If the carriage is dist mm away from 0, what are its coordinates, adjusted for tower lean?
+void LinearDeltaSolution::get_tower_xyz_for_dist(uint8_t tower, float xyz[], float dist)
+{
+
+    float *vec;
+
+    switch(tower) {
+
+        case X:
+            xyz[X] = delta_tower1_x;
+            xyz[Y] = delta_tower1_y;
+            vec = tower1_vector;
+            break;
+
+        case Y:
+            xyz[X] = delta_tower2_x;
+            xyz[Y] = delta_tower2_y;
+            vec = tower2_vector;
+            break;
+
+        case Z:
+            xyz[X] = delta_tower3_x;
+            xyz[Y] = delta_tower3_y;
+            vec = tower3_vector;
+            break;
+
+        default:
+            // Would be nice to print an error here, but it would flood the serial line and lock up the board.
+            // Whatever you do with the rest of the code, make sure the instruction pointer never winds up here.
+            xyz[X] = 100;  // 100 seems less dangerous than whatever random noise is in xy[]
+            xyz[Y] = 100;  // Should result in very strange motion, which should alert developer that there's a problem
+            return;
+
+    } // switch(tower)
+
+    xyz[X] += vec[X] * dist;
+    xyz[Y] += vec[Y] * dist;
+    xyz[Z] = vec[Z] * dist;
+
+}
+
+
 bool LinearDeltaSolution::set_optional(const arm_options_t& options) {
 
     for(auto &i : options) {
@@ -134,6 +272,17 @@ bool LinearDeltaSolution::set_optional(const arm_options_t& options) {
             case 'D': tower1_angle = i.second; break;
             case 'E': tower2_angle = i.second; break;
             case 'F': tower3_angle = i.second; break;
+            
+            // Already taken or invalid: R, T, Z
+            case 'H': tower1_vector[X] = i.second; break;
+            case 'I': tower1_vector[Y] = i.second; break;
+            case 'J': tower1_vector[Z] = i.second; break; // R is already taken
+            case 'N': tower2_vector[X] = i.second; break; // T is already taken
+            case 'O': tower2_vector[Y] = i.second; break;
+            case 'P': tower2_vector[Z] = i.second; break;
+            case 'W': tower3_vector[X] = i.second; break;
+            case 'X': tower3_vector[Y] = i.second; break;
+            case 'Y': tower3_vector[Z] = i.second; break; // Z is already taken
         }
     }
     init();
@@ -154,6 +303,17 @@ bool LinearDeltaSolution::get_optional(arm_options_t& options) {
         options['D'] = this->tower1_angle;
         options['E'] = this->tower2_angle;
         options['F'] = this->tower3_angle;
+
+        options['H'] = this->tower1_vector[X];
+        options['I'] = this->tower1_vector[Y];
+        options['J'] = this->tower1_vector[Z];
+        options['N'] = this->tower2_vector[X];
+        options['O'] = this->tower2_vector[Y];
+        options['P'] = this->tower2_vector[Z];
+        options['W'] = this->tower3_vector[X];
+        options['X'] = this->tower3_vector[Y];
+        options['Y'] = this->tower3_vector[Z];
+
     }
 
     return true;
--- src/modules/robot/arm_solutions/LinearDeltaSolution.h
+++ src/modules/robot/arm_solutions/LinearDeltaSolution.h
@@ -3,13 +3,22 @@
 #include "libs/Module.h"
 #include "BaseSolution.h"
 
+// You have to include this to get memcpy
+#include <cstring>
+
+#define X 0
+#define Y 1
+#define Z 2
+
 class Config;
 
 class LinearDeltaSolution : public BaseSolution {
+
     public:
         LinearDeltaSolution(Config*);
         void cartesian_to_actuator( float[], float[] );
         void actuator_to_cartesian( float[], float[] );
+        void get_tower_xyz_for_dist(uint8_t tower, float xyz[], float dist);
 
         bool set_optional(const arm_options_t& options);
         bool get_optional(arm_options_t& options);
@@ -21,17 +30,29 @@ class LinearDeltaSolution : public BaseSolution {
         float arm_radius;		// Same as "delta radius" in other delta robot control software
         float arm_length_squared;
 
+        // { X, Y } location of each tower's top
         float delta_tower1_x;
         float delta_tower1_y;
         float delta_tower2_x;
         float delta_tower2_y;
         float delta_tower3_x;
         float delta_tower3_y;
+
+        // Distance-from-center offset for each tower
         float tower1_offset;
         float tower2_offset;
         float tower3_offset;
+
+        // Offset from ideal angle (e.g.: Z tower is 90 degrees + tower3_angle)
         float tower1_angle;
         float tower2_angle;
         float tower3_angle;
+
+        // Tower lean: Unit vector pointing from top of tower, to bottom of tower :~)
+        float tower1_vector[3];
+        float tower2_vector[3];
+        float tower3_vector[3];
+
+
 };
 #endif // LINEARDELTASOLUTION_H
