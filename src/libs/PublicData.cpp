42fcc65182b9a8e6113828d48d06f34cb01c68e7
Jim Morris <morris@wolfman.com>
2015-09-12 01:25:55 -0700

    Fix bug in publicdata

--- src/libs/PublicData.cpp
+++ src/libs/PublicData.cpp
@@ -6,7 +6,7 @@ bool PublicData::get_value(uint16_t csa, uint16_t csb, uint16_t csc, void *data)
     PublicDataRequest pdr(csa, csb, csc);
     pdr.set_data_ptr(data); // the caller may have put a placeholder for the returned data here
     THEKERNEL->call_event(ON_GET_PUBLIC_DATA, &pdr );
-    if(pdr.has_returned_data()) {
+    if(pdr.is_taken() && pdr.has_returned_data()) {
         // the callee set the returned data pointer
         *(void**)data= pdr.get_data_ptr();
     }
