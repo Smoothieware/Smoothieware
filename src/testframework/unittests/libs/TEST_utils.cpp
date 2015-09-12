e6b3d64c67c9a0aa2ffbfd845ef53751887c64e9
Jim Morris <morris@wolfman.com>
2015-09-12 14:29:29 -0700

    Add test framework using easyunit add unit test for TemperatureSwitch add unit test for some utils functions as examples added unregister_for_event() to Kernel so modules can be tested Hack FirmConfigSource to allow configs to be specified in memory for testing allow config to be deleted Modified Rakefile to allow buildong of test framework with rake testing=1 modify makefiles to ignore the src/testframework for builds

---
+++ src/testframework/unittests/libs/TEST_utils.cpp
@@ -0,0 +1,36 @@
+#include "utils.h"
+
+#include <vector>
+#include <stdio.h>
+
+#include "easyunit/test.h"
+
+TEST(UtilsTest,split)
+{
+    const char *s= "one two three";
+    std::vector<std::string> v= split(s, ' ');
+    ASSERT_TRUE(v.size() == 3);
+    ASSERT_TRUE(v[0] == "one");
+    ASSERT_TRUE(v[1] == "two");
+    ASSERT_TRUE(v[2] == "three");
+}
+
+TEST(UtilsTest,split_empty_string)
+{
+    const char *s= "";
+    std::vector<std::string> v= split(s, ' ');
+
+     ASSERT_TRUE(v.size() == 1);
+     ASSERT_TRUE(v[0].empty());
+     ASSERT_TRUE(v[0] == "");
+}
+
+TEST(UtilsTest,parse_number_list)
+{
+    const char *s= "1.1,2.2,3.3";
+    std::vector<float> v= parse_number_list(s);
+    ASSERT_TRUE(v.size() == 3);
+    ASSERT_TRUE(v[0] == 1.1F);
+    ASSERT_TRUE(v[1] == 2.2F);
+    ASSERT_TRUE(v[2] == 3.3F);
+}
