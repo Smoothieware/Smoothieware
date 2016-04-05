db5b3d381b9a6d799d807422f227352a57d802aa
Michal Wojciechowski <m.wojciechowski@tekno.katowice.pl>
2016-04-05 23:53:09 +0200

    final fix add hours

--- src/modules/utils/panel/screens/WatchScreen.cpp
+++ src/modules/utils/panel/screens/WatchScreen.cpp
@@ -253,7 +253,7 @@ void WatchScreen::display_menu_line(uint16_t line)
             break;
         }
         case 1: THEPANEL->lcd->printf("X%4d Y%4d Z%7.2f", (int)round(this->pos[0]), (int)round(this->pos[1]), this->pos[2]); break;
-		case 2: THEPANEL->lcd->printf("%3d%% %2lu:%02lu %3u%% sd", this->current_speed, this->last_time / 60, this->last_time % 60, this->sd_pcnt_played); break;
+		case 2: THEPANEL->lcd->printf("%3d%% %2lu:%02lu:%02lu %3u%% sd", this->current_speed, this->last_time / 3600, this->last_time % 3600 / 60, this->last_time % 60, this->sd_pcnt_played); break;
 		case 3: THEPANEL->lcd->printf("%19s", this->get_status()); break;
     }
 }
