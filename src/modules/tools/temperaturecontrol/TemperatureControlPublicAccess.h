a9f4149f2d6bcb6f70ad59777217f477f2540195
Michal Wojciechowski <m.wojciechowski@tekno.katowice.pl>
2016-04-06 14:10:59 +0200

    second attempt - layout mod

--- src/modules/tools/temperaturecontrol/TemperatureControlPublicAccess.h
+++ src/modules/tools/temperaturecontrol/TemperatureControlPublicAccess.h
@@ -8,7 +8,9 @@
 // addresses used for public data access
 #define temperature_control_checksum      CHECKSUM("temperature_control")
 #define hotend_checksum                   CHECKSUM("hotend")
+#define hotend2_checksum                  CHECKSUM("hotend2")
 #define bed_checksum                      CHECKSUM("bed")
+#define pcb_checksum                      CHECKSUM("pcb")
 #define current_temperature_checksum      CHECKSUM("current_temperature")
 #define target_temperature_checksum       CHECKSUM("target_temperature")
 #define temperature_pwm_checksum          CHECKSUM("temperature_pwm")
