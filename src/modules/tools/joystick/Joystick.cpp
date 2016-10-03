be86714e9b55ddf9f3c61ee725196dee2ff65432
skaterzero807 <nicholsbv@gmail.com>
2016-10-02 23:20:21 -0400

    Test, commit deletions for -rewrite branch
    Testing to see if commiting deletions to the -rewrite branch leaves the
    original branch unharmed

--- src/modules/tools/joystick/Joystick.cpp
+++ src/modules/tools/joystick/Joystick.cpp
@@ -63,7 +63,6 @@ void Joystick::on_module_loaded()
     //register for some events with the kernel
     this->register_for_event(ON_GCODE_RECEIVED); //ask to be notified when gcode is received, it might be for us
     THEKERNEL->slow_ticker->attach(100, this, &Joystick::update_tick); //not sure what this was doing
-    THEKERNEL->step_ticker->register_acceleration_tick_handler([this](){acceleration_tick(); }); //also not sure what this was doing
 
     /*
     TODO:
@@ -74,10 +73,16 @@ void Joystick::on_module_loaded()
         selectable speed settings, fast/slow (handle a custom M code for this? then switch module can make the selection via gcode)
 
         possible future change: make one module for reading joystick values and mapping them between -1 and 1
-        then have a "Jog" module which manages the motor movements. the user can choose which applicable module to obtain the -1 to 1 readings
+        Make Joystick module have multiple instances like the extruder module, one for each possible analog axis
+        Make Joystick module share its position using SharedData? then any interested module can use the ON_SET_PUBLIC_DATA event to get the reading
+        OR Joystick module is able to respond to a SharedData request, any interested module asks for the data
+
+        have a "Jog" module which manages the motor movements given an input from -1 to 1. Can be configured to use any module,
+        just needs to be a module that supports the public data getting/setting
     */
 
     //read position repeatedly to get an average for "stationary" joystick values, maybe need to register a slow timer with the kernel for this
+    //probably best to have a "startup time" where the module is inactive, and is obtaining readings to average, only if this "startup time" is enabled in config
     /*
     int read_sum[2] = {0, 0};
 
@@ -95,12 +100,8 @@ void Joystick::on_module_loaded()
 void Joystick::on_config_reload(void *argument)
 {
     //read config file values for this module
-    this->max_speed = THEKERNEL->config->value(default_seek_rate_checksum )->by_default(10.0f )->as_number();
-    this->max_speed = THEKERNEL->config->value(joystick_checksum, max_speed_checksum )->by_default(this->max_speed )->as_number();
-    this->dead_zone = THEKERNEL->config->value(joystick_checksum, dead_zone_checksum )->by_default(0.002f )->as_number();
     this->zero_offset[0] = THEKERNEL->config->value(joystick_checksum, zero_offset_horizontal_checksum )->by_default(2047 )->as_number();
     this->zero_offset[1] = THEKERNEL->config->value(joystick_checksum, zero_offset_vertical_checksum )->by_default(2047 )->as_number();
-    this->nonlinearity = THEKERNEL->config->value(joystick_checksum, nonlinearity_checksum )->by_default(1.0f )->as_number();
 
     // pins for ADC readings
     this->axis_pin[0].from_string(THEKERNEL->config->value(joystick_checksum, horizontal_pin_checksum )->required()->as_string());
@@ -108,14 +109,6 @@ void Joystick::on_config_reload(void *argument)
 
     this->axis_pin[1].from_string(THEKERNEL->config->value(joystick_checksum, vertical_pin_checksum )->required()->as_string());
     THEKERNEL->adc->enable_pin(&axis_pin[1]);
-
-    //pins for endstops (currently having to check manually before issuing stepper move function)
-    this->endstop_pin[0].from_string( THEKERNEL->config->value(alpha_min_endstop_checksum          )->by_default("nc" )->as_string())->as_input();
-    this->endstop_pin[1].from_string( THEKERNEL->config->value(beta_min_endstop_checksum           )->by_default("nc" )->as_string())->as_input();
-    this->endstop_pin[2].from_string( THEKERNEL->config->value(gamma_min_endstop_checksum          )->by_default("nc" )->as_string())->as_input();
-    this->endstop_pin[3].from_string( THEKERNEL->config->value(alpha_max_endstop_checksum          )->by_default("nc" )->as_string())->as_input();
-    this->endstop_pin[4].from_string( THEKERNEL->config->value(beta_max_endstop_checksum           )->by_default("nc" )->as_string())->as_input();
-    this->endstop_pin[5].from_string( THEKERNEL->config->value(gamma_max_endstop_checksum          )->by_default("nc" )->as_string())->as_input();
 }
 
 //read joystick position, takes multiple readings and removes outliers (> 1 stdev from mean)
@@ -178,7 +171,7 @@ float* Joystick::get_normalized(int* pos)
         norm[i] = pos_zero[i] / (float) min(4095 - zero_offset[i], zero_offset[i]);
 
         //constrain to within +/- 1
-        if(norm[i] < -1 || norm[i] > 1){
+        if(abs(norm[i]) > 1){
             norm[i] = sign(norm[i]);
         }
         
@@ -187,108 +180,11 @@ float* Joystick::get_normalized(int* pos)
     return norm;
 }
 
-//map a position from -1 to 1 into a speed from -max to max
-float Joystick::get_speed(float pos){
-    float speed;
-
-    //apply non-linear scaling to position
-    speed = (sign(pos) * pow(abs(pos), nonlinearity)) * max_speed;
-
-    //zero out in dead zone
-    if(abs(pos) <= dead_zone){
-        speed = 0;
-    }
-
-    return speed;
-}
-
-void Joystick::on_gcode_received(void *argument)
-{
-    //testing code here
-    //print out parameters
-    //int* pos = read_pos();
-    //THEKERNEL->streams->printf("%+0.3f, %+0.3f          Max: %f,  Dead: %f,  Zero: %d, %d, Pw: %f,  ADCX: %d,  ADCY: %d\n", this->position[0], this->position[1], max_speed, dead_zone, zero_offset[0], zero_offset[1], nonlinearity, pos[0], pos[1]);
-}
 
 uint32_t Joystick::update_tick(uint32_t dummy)
 {
     //update the current joystick position
     this->position = get_normalized(read_pos());
 
-    //don't bother doing anything if the robot is busy with commands
-    if(!THEKERNEL->conveyor->is_queue_empty()) return 0;
-
-    //update stepper's target speed
-    for(int c = 0; c < 2; c++){
-        this->target_feedrate[c] = get_speed(this->position[c]) * STEPS_PER_MM(c);
-        
-        //stop here if any endstop safety checks fail (only checking in direction of travel)
-        if(this->endstop_pin[c + ((target_feedrate[c]<0)? 0 : 3)].get()) continue;
-        
-        //enable the stepper if the target speed is non-zero
-        if(!this->enabled[c] && target_feedrate[c] != 0){
-            this->enabled[c] = true;
-            THEKERNEL->stepper->turn_enable_pins_on();
-        }
-    }
-
-    //if jogging, update the robot's last milestone (i.e. position) to the steppers' current positions
-    THEKERNEL->robot->reset_position_from_current_actuator_position();
-
     return 0;
-}
-
-// Called periodically to change the speed to match acceleration
-void Joystick::acceleration_tick(void)
-{
-    //update enabled steppers' speeds
-    for(int c = 0; c < 2; c++){
-        if(enabled[c]) accelerate(c);
-    }
-
-    return;
-}
-
-//acceleration calculation, just need to set this->current_feedrate
-void Joystick::accelerate(int c)
-{   
-    float current_rate = (direction[c])? STEPPER[c]->get_steps_per_second() : -1*(STEPPER[c]->get_steps_per_second());
-    if(!STEPPER[c]->is_moving()) current_rate = 0;
-
-    float target_rate = this->target_feedrate[c];
-    float old_rate = current_rate;
-
-    // Z may have a different acceleration to X and Y
-    float acc = (c==Z_AXIS) ? THEKERNEL->planner->get_z_acceleration() : THEKERNEL->planner->get_acceleration();
-    float rate_change = (acc / THEKERNEL->acceleration_ticks_per_second) * STEPS_PER_MM(c);
-
-    //update the current rate using the acceleration
-    if( current_rate <= target_rate ) {
-        current_rate = min( target_rate, current_rate + rate_change );
-    } else {
-        current_rate = max( target_rate, current_rate - rate_change );
-    }
-
-    //check if current rate is zero, stop motors if so
-    if(current_rate == 0){
-        STEPPER[c]->move(0, 0);
-        enabled[c] = false;
-
-    } else {
-        //check if switched sign from last speed to new speed, or last speed was 0
-        if(sign(old_rate) != sign(current_rate) || old_rate == 0){
-            //update direction
-            direction[c] = current_rate>0;
-
-            //then command a motor movement
-            // not a block move so disable the last tick setting? (not sure about this line, copied from example)
-            STEPPER[c]->set_moved_last_block(false);
-
-            //start motor moving (commanded distance is 100 meters, should be large enough to ensure the motor won't stop unexpectedly)
-            STEPPER[c]->move(direction[c], 100000 * STEPS_PER_MM(c), 0);
-        }
-    
-        //set motor steps per second
-        STEPPER[c]->set_speed(abs(current_rate));
-    }
 }
--- src/modules/tools/joystick/Joystick.h
+++ src/modules/tools/joystick/Joystick.h
@@ -8,8 +8,9 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
 
-#include "libs/Pin.h"
 #include "RingBuffer.h"
+
+class Pin;
 
 #define QUEUE_LEN 32
 
@@ -21,22 +22,13 @@ class Joystick : public Module {
         void on_module_loaded();
         void on_config_reload(void* argument);
         uint32_t update_tick(uint32_t);
-        void on_gcode_received(void* argument);
 
         int* read_pos();
         float* get_normalized(int* pos);
-        float get_speed(float pos);
-
-        void acceleration_tick(void);
-        void accelerate(int c);
 
 private:
         Pin axis_pin[2];
-        Pin endstop_pin[6];
-        float max_speed;
-        float dead_zone;
         int zero_offset [2] = {};
-        float nonlinearity;
         int averaging_length;
 
         float* position;
