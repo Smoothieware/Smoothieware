d791c523e21448989604618b0393b48d1d53ae79
root <root@localhost.localdomain>
2016-10-16 03:26:43 +0200

    ifnder NO_RTD_CURVE added

--- src/modules/tools/temperaturecontrol/pt100.cpp
+++ src/modules/tools/temperaturecontrol/pt100.cpp
@@ -19,11 +19,13 @@
 #define slope_checksum                     CHECKSUM("slope")              // We are looking RTD as "linear" so tune your
 #define yintercept_checksum                CHECKSUM("yintercept")         // SLOPE and Y-Intercept to have minimal error
                                                                           // in the region you use 
+
+#ifndef NO_RTD_CURVE
 #define pt100A_checksum                    CHECKSUM("pt100_a")            // A value
 #define pt100B_checksum                    CHECKSUM("pt100_b")            // B value
 #define pt100R0_checksum                   CHECKSUM("pt100_r0")           // R0 value
 #define pt100amptype_checksum              CHECKSUM("pt100_amptype")      // Amp type between RTD and ADC input
-                                                                          // attm only supported ones are 
+#endif                                                                    // attm only supported ones are 
                                                                           // 0: e3d pt100 amp http://wiki.e3d-online.com/wiki/E3D_PT100_Amplifier_Documentation
 
 PT100::PT100()
@@ -35,8 +37,10 @@ PT100::PT100()
     this->max_temp= 0;
     this->m = 0;             //use as slope or A 
     this->b = 0;             //use as yintercept or B
+#ifndef NO_RTD_CURVE
     this->r0= 0;             // R0 - temp of the RTD and 0C (100 for PT100)
     this->use_linear= true;  // true: use linear, false: use fancy math
+#endif
 }
 
 PT100::~PT100()
@@ -48,18 +52,21 @@ void PT100::UpdateConfig(uint16_t module_checksum, uint16_t name_checksum)
     this->thermistor_pin.from_string(THEKERNEL->config->value(module_checksum, name_checksum, thermistor_pin_checksum )->required()->as_string());
     this->ampmod1_pin.from_string(   THEKERNEL->config->value(module_checksum, name_checksum, ampmod1_pin_checksum    )->by_default("nc")->as_string());
     this->ampmod2_pin.from_string(   THEKERNEL->config->value(module_checksum, name_checksum, ampmod2_pin_checksum    )->by_default("nc")->as_string());
+#ifndef NO_RTD_CURVE
     this->use_linear = THEKERNEL->config->value(module_checksum, name_checksum,  pt100linear_checksum)->by_default(1)->as_number() > 0;
 
     if (this->use_linear){
+#endif
       this->m = THEKERNEL->config->value(module_checksum, name_checksum, slope_checksum      )->by_default(  0.0257604875F )->as_number(); // default value for UP! 
       this->b = THEKERNEL->config->value(module_checksum, name_checksum, yintercept_checksum )->by_default(-18.54F         )->as_number(); // PT100 in the hotend
+#ifndef NO_RTD_CURVE
     }else{
       this->m = THEKERNEL->config->value(module_checksum, name_checksum, pt100A_checksum )->by_default( 3.9083E-3F )->as_number(); // ALFA (for PT100 ITS-90 it is 3.9083E-3 )
       this->b = THEKERNEL->config->value(module_checksum, name_checksum, pt100B_checksum )->by_default( 5.775E-7F  )->as_number(); // BETA for the RTD (for PT100 ITS-90 it is 5.775E-7 )
       this->r0= THEKERNEL->config->value(module_checksum, name_checksum, pt100R0_checksum)->by_default( 100        )->as_number(); // R0 - resistance on 0C (for PT100 it is 100)
       this->amptype= THEKERNEL->config->value(module_checksum, name_checksum, pt100amptype_checksum)->by_default(0 )->as_number(); // Amp type
     }
-
+#endif
     THEKERNEL->adc->enable_pin(&thermistor_pin);
 
     if(this->ampmod1_pin.connected()){
@@ -105,13 +112,16 @@ float PT100::adc_value_to_temperature(uint32_t adc_value)
 
     float t;
 
+#ifndef NO_RTD_CURVE
     if (this->use_linear){
         // We are using a very short range of temperatures for 3D printing
         // so looking at 150-350C span any RTD is almost linear hence we
         // use the simple linear equation allowing slope and y-intersect to 
         // be configurable so user can tune for particular RTD and tune for 
         // minimal error on the required span
+#endif
         t = adc_value * this->m + this->b;
+#ifndef NO_RTD_CURVE
     } else {
         // proper RTD calculation should be like this
         // but depending on how the RTD is connected, is there 
@@ -136,6 +146,7 @@ float PT100::adc_value_to_temperature(uint32_t adc_value)
         }
         t = (-1 * this->r0 * this->m + sqrtf(this->r0 * this->r0 * + this->m * this->m - 4 * this->r0 * this->b * (this->r0 - t ))) / (2 * this->r0 * this->b);
     }
+#endif
     return t;
 }
 
