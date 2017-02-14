48591be3f484b8dc658b77d849490862d851f8a3
Brendan Nichols <nicholsbv@gmail.com>
2017-02-13 19:16:02 -0500

    fix header initializations to use curly braces

--- src/modules/tools/jogger/Jogger.h
+++ src/modules/tools/jogger/Jogger.h
@@ -29,26 +29,28 @@ public:
 private:
     float get_speed(float pos); //function to return a speed given a unitless joystick reading
 
-    float max_speed = 10.0f; //maximum allowed speed (defaults to slow 10 mm/min)
-    float dead_zone = 0.002f; //distance from 0 in which small joystick movement is ignored
-    float nonlinearity = 1.0f; //nonlinearity parameter for scaling of joystick values
+    float max_speed{ 10.0f }; //maximum allowed speed (defaults to slow 10 mm/min)
+    float dead_zone{ 0.002f }; //distance from 0 in which small joystick movement is ignored
+    float nonlinearity{ 1.0f }; //nonlinearity parameter for scaling of joystick values
 
-    float position[NUM_JOG_AXES] = {}; //keeps track of the joystick positions for each axis
-    float target_speed[NUM_JOG_AXES] = {}; //keeps track of the target speed of each axis
-    char axis_letter[NUM_JOG_AXES] = {}; //keeps track of which machine axis is controlled by each joystick axis
+    float position[NUM_JOG_AXES]; //keeps track of the joystick positions for each axis
+    float target_speed[NUM_JOG_AXES]; //keeps track of the target speed of each axis
+    char axis_letter[NUM_JOG_AXES]; //keeps track of which machine axis is controlled by each joystick axis
     std::vector<std::string> jog_axes; //stores the list of machine axes controlled by the joystick
-    unsigned int axis_index = 0; //stores which item in the list of machine axis configs is in use
-    bool is_active = false; //keeps track of whether the joystick is being moved
-    bool is_jogging = false; //keeps track of whether the robot is jogging
-    bool do_reading = false; //flag for when a joystick reading should occur
+    unsigned int axis_index{ 0 }; //stores which item in the list of machine axis configs is in use
+    struct {
+        bool is_active:1; //keeps track of whether the joystick is being moved
+        bool is_jogging:1; //keeps track of whether the robot is jogging
+        bool do_reading:1; //flag for when a joystick reading should occur
+    };
 
-    int refresh_rate = 100; //number of jog speed updates per second (absolute max = base_step_frequency (100 kHz))
-    int segment_frequency = 100; //approximate number of segments per second during jogging
+    int refresh_rate{ 100 }; //number of jog speed updates per second (absolute max = base_step_frequency (100 kHz))
+    int segment_frequency{ 100 }; //approximate number of segments per second during jogging
 
-    uint16_t m_code_set = 777; //m-code number to use when setting jog axes
-    uint16_t m_code_toggle = 778; //m-code number to use when toggling jog axes
+    uint16_t m_code_set{ 777 }; //m-code number to use when setting jog axes
+    uint16_t m_code_toggle{ 778 }; //m-code number to use when toggling jog axes
 
-    uint16_t axis_data_source[NUM_JOG_AXES] = {}; //checksums of the joystick module name from which to obtain data for axis n
+    uint16_t axis_data_source[NUM_JOG_AXES]; //checksums of the joystick module name from which to obtain data for axis n
 
     void update_Joystick(void);
     void update_Jogging(void);
--- src/modules/tools/joystick/JoystickAxis.h
+++ src/modules/tools/joystick/JoystickAxis.h
@@ -20,30 +20,29 @@ class JoystickAxis : public Module {
 
         void on_module_loaded();
         void on_config_reload(void* argument);
-        //void on_gcode_received(void* argument);
         void on_get_public_data(void* argument);
         uint32_t update_tick(uint32_t);
 
-        float read_pos(); //read the ADC voltage (0 to ADC_VREF)
-        float get_normalized(float pos); //get the scaled value (-1 to 1) from the ADC value
-
     private:
-        uint16_t name_checksum = 0; //the "name" of this instance (only a checksum of the text however)
+        uint16_t name_checksum{ 0 }; //the "name" of this instance (only a checksum of the text however)
 
-        float position = 0; //the scaled axis value (-1 to 1)
+        float position{ 0 }; //the scaled axis value (-1 to 1)
 
         Pin axis_pin; //which pin on the smoothie will provide the analog input
-        float zero_offset = 0; //the voltage which corresponds to zero on the scaled axis
-        float endpoint = ADC_VREF; //the voltage which corresponds to either 1 or -1 (depends on its size relative to zero_offset)
+        float zero_offset{ 0 }; //the voltage which corresponds to zero on the scaled axis
+        float endpoint{ ADC_VREF }; //the voltage which corresponds to either 1 or -1 (depends on its size relative to zero_offset)
 
-        bool auto_zero = false; //whether or not to automatically determine the zero-offset at startup
-        bool in_startup = true; //whether the module is currently in startup mode
-        float last_reading = 0.0f; //stores the last ADC reading
-        int startup_time = 1000; //number of milliseconds to spend determining the zero-offset before normal functionality
-        int startup_intervals = 0; //keeps track of how many refresh intervals have passed in the startup time
-        float startup_sum = 0.0f; //keeps track of the sum of ADC readings during startup
+        bool auto_zero{ false }; //whether or not to automatically determine the zero-offset at startup
+        bool in_startup{ true }; //whether the module is currently in startup mode
+        float last_reading{ 0.0f }; //stores the last ADC reading
+        int startup_time{ 1000 }; //number of milliseconds to spend determining the zero-offset before normal functionality
+        int startup_intervals{ 0 }; //keeps track of how many refresh intervals have passed in the startup time
+        float startup_sum{ 0.0f }; //keeps track of the sum of ADC readings during startup
 
-        int refresh_rate = 10; //number of ADC readings per second (absolute max = base_stepping_frequency (100 kHz))
+        int refresh_rate{ 10 }; //number of ADC readings per second (absolute max = base_stepping_frequency (100 kHz))
+
+        float read_pos(); //read the ADC voltage (0 to ADC_VREF)
+        float get_normalized(float pos); //get the scaled value (-1 to 1) from the ADC value
 };
 
 #endif //JOYSTICKAXIS_H
