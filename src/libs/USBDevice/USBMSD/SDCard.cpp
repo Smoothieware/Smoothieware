9bf3a51ce7682eb63470af93ef461482d0d81f9a
Andy Sigler <andrewsigler1@gmail.com>
2017-11-26 17:12:35 -0500

    removes all handshakes with SD card

--- src/libs/USBDevice/USBMSD/SDCard.cpp
+++ src/libs/USBDevice/USBMSD/SDCard.cpp
@@ -124,7 +124,7 @@
 
 static const uint8_t OXFF = 0xFF;
 
-#define SD_COMMAND_TIMEOUT 100
+#define SD_COMMAND_TIMEOUT 0
 
 SDCard::SDCard(PinName mosi, PinName miso, PinName sclk, PinName cs) :
   _spi(mosi, miso, sclk), _cs(cs) {
