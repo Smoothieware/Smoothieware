573ed7c0c719008075acb1d36850e718a2707372
Jim Morris <morris@wolfman.com>
2018-03-13 18:48:28 +0000

    call on_idle
    fixes issue #1286

--- src/modules/tools/temperaturecontrol/TemperatureControl.cpp
+++ src/modules/tools/temperaturecontrol/TemperatureControl.cpp
@@ -97,13 +97,13 @@ void TemperatureControl::on_module_loaded()
     // Register for events
     this->register_for_event(ON_GCODE_RECEIVED);
     this->register_for_event(ON_GET_PUBLIC_DATA);
+    this->register_for_event(ON_IDLE);
 
     if(!this->readonly) {
         this->register_for_event(ON_SECOND_TICK);
         this->register_for_event(ON_MAIN_LOOP);
         this->register_for_event(ON_SET_PUBLIC_DATA);
         this->register_for_event(ON_HALT);
-        this->register_for_event(ON_IDLE);
     }
 }
 
