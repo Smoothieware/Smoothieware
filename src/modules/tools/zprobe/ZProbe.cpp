84484f75ee50d5f3640b915cbfb45ba019d488c8
James Richters <james@productionautomation.net>
2018-04-23 12:49:29 -0400

    Remove condition and set invet_probe to false for all G38 gcodes-- it's already set to false for all Gcodes from 29 to 32 on line 259

--- src/modules/tools/zprobe/ZProbe.cpp
+++ src/modules/tools/zprobe/ZProbe.cpp
@@ -326,6 +326,7 @@ void ZProbe::on_gcode_received(void *argument)
 
     } else if(gcode->has_g && gcode->g == 38 ) { // G38.2 Straight Probe with error, G38.3 straight probe without error
         // linuxcnc/grbl style probe http://www.linuxcnc.org/docs/2.5/html/gcode/gcode.html#sec:G38-probe
+        invert_probe = false;
         if(gcode->subcode < 2 || gcode->subcode > 5) {
             gcode->stream->printf("error:Only G38.2 to G38.5 are supported\n");
             return;
@@ -368,10 +369,6 @@ void ZProbe::on_gcode_received(void *argument)
         }
 
         probe_XYZ(gcode, x, y, z);
-
-        if(gcode->subcode == 4 || gcode->subcode == 5) {
-            invert_probe = false;
-        }
 
         return;
 
