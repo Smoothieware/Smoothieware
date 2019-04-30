82dd6c222ceaeb8ca55e21510d68e6b65f884286
Jim Morris <morris@wolfman.com>
2019-04-30 16:21:01 +0100

    Allow $J to handle multiple axis at one time so $J X1 Y1 is now handled

--- src/modules/utils/simpleshell/SimpleShell.cpp
+++ src/modules/utils/simpleshell/SimpleShell.cpp
@@ -1185,51 +1185,73 @@ void SimpleShell::test_command( string parameters, StreamOutput *stream)
 
 void SimpleShell::jog(string parameters, StreamOutput *stream)
 {
-    // $J X0.1 F0.5
+    // $J X0.1 [Y0.2] [F0.5]
     int n_motors= THEROBOT->get_number_registered_motors();
 
     // get axis to move and amount (X0.1)
-    // for now always 1 axis
-    size_t npos= parameters.find_first_of("XYZABC");
-    if(npos == string::npos) {
-        stream->printf("usage: $J X0.01 [F0.5] - axis can be one of XYZABC, optional speed is scale of max_rate\n");
-        return;
-    }
+    // may specify multiple axis
 
-    string s = parameters.substr(npos);
-    if(s.empty() || s.size() < 2) {
-        stream->printf("usage: $J X0.01 [F0.5]\n");
-        return;
-    }
-    char ax= toupper(s[0]);
-    uint8_t a= ax >= 'X' ? ax - 'X' : ax - 'A' + 3;
-    if(a >= n_motors) {
-        stream->printf("error:bad axis\n");
-        return;
-    }
-
-    float d= strtof(s.substr(1).c_str(), NULL);
-
+    float rate_mm_s= NAN;
+    float scale= 1.0F;
     float delta[n_motors];
     for (int i = 0; i < n_motors; ++i) {
         delta[i]= 0;
     }
-    delta[a]= d;
 
-    // get speed scale
-    float scale= 1.0F;
-    npos= parameters.find_first_of("F");
-    if(npos != string::npos && npos+1 < parameters.size()) {
-        scale= strtof(parameters.substr(npos+1).c_str(), NULL);
+    // $J is first parameter
+    shift_parameter(parameters);
+    if(parameters.empty()) {
+        stream->printf("usage: $J X0.01 [F0.5] - axis can be XYZABC, optional speed is scale of max_rate\n");
+        return;
     }
 
-    float rate_mm_s= THEROBOT->actuators[a]->get_max_rate() * scale;
-    THEROBOT->delta_move(delta, rate_mm_s, n_motors);
+    while(!parameters.empty()) {
+        string p= shift_parameter(parameters);
 
+        char ax= toupper(p[0]);
+        if(ax == 'F') {
+            // get speed scale
+            scale= strtof(p.substr(1).c_str(), NULL);
+            continue;
+        }
+
+        if(!((ax >= 'X' && ax <= 'Z') || (ax >= 'A' && ax <= 'C'))) {
+            stream->printf("error:bad axis %c\n", ax);
+            return;
+        }
+
+        uint8_t a= ax >= 'X' ? ax - 'X' : ax - 'A' + 3;
+        if(a >= n_motors) {
+            stream->printf("error:axis out of range %c\n", ax);
+            return;
+        }
+
+        delta[a]= strtof(p.substr(1).c_str(), NULL);
+    }
+
+    // select slowest axis rate to use
+    bool ok= false;
+    for (int i = 0; i < n_motors; ++i) {
+        if(delta[i] != 0) {
+            ok= true;
+            if(isnan(rate_mm_s)) {
+                rate_mm_s= THEROBOT->actuators[i]->get_max_rate();
+            }else{
+                rate_mm_s = std::min(rate_mm_s, THEROBOT->actuators[i]->get_max_rate());
+            }
+            //hstream->printf("%d %f F%f\n", i, delta[i], rate_mm_s);
+        }
+    }
+    if(!ok) {
+        stream->printf("error:no delta jog specified\n");
+        return;
+    }
+
+    //stream->printf("F%f\n", rate_mm_s*scale);
+
+    THEROBOT->delta_move(delta, rate_mm_s*scale, n_motors);
     // turn off queue delay and run it now
     THECONVEYOR->force_queue();
-
-    //stream->printf("Jog: %c%f F%f\n", ax, d, scale);
 }
 
 void SimpleShell::help_command( string parameters, StreamOutput *stream )
