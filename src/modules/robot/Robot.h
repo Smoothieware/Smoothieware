1e791c37b7e0146af2e0a102b6d9a8eb4dffe225
Jim Morris <morris@wolfman.com>
2020-05-01 12:51:45 +0100

    minor whitespace fix

--- src/modules/robot/Robot.h
+++ src/modules/robot/Robot.h
@@ -119,7 +119,7 @@ class Robot : public Module {
         int get_active_extruder() const;
 
         std::array<wcs_t, MAX_WCS> wcs_offsets; // these are persistent once saved with M500
-        uint8_t current_wcs{0}; // 0 means G54 is enabled this is persistent once saved with M500
+        uint8_t current_wcs{0}; // 0 means G54 is enabled this is persistent once saved with M500
         wcs_t g92_offset;
         wcs_t tool_offset; // used for multiple extruders, sets the tool offset for the current extruder applied first
         std::tuple<float, float, float, uint8_t> last_probe_position{0,0,0,0};
