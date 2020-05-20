401452e825609972b7065b576b19a13d6ed9221a
Jim Morris <morris@wolfman.com>
2020-05-20 21:04:18 +0100

    allow player to handle files terminated with \r\n

--- src/modules/utils/player/Player.cpp
+++ src/modules/utils/player/Player.cpp
@@ -427,7 +427,11 @@ void Player::on_main_loop(void *argument)
                     continue;
                 }
                 if(len == 1) continue; // empty line
-
+                if(buf[len - 2] == '\r') {
+                    // \r\n terminated ignore \r
+                    len -=1;
+                    if(len == 1) continue; // empty line
+                }
                 if(this->current_stream != nullptr) {
                     this->current_stream->printf("%s", buf);
                 }
