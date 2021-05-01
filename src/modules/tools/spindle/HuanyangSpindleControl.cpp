e24ee4e02d4220f5698b1580285422be1ee8ea9c
Rickishier <34369476+Rickishier@users.noreply.github.com>
2021-05-01 13:34:18 +0200

    Update HuanyangSpindleControl.cpp
    made it work for a Sako SKI780
    it now can tell the VFD to start/stop and control the speed

--- src/modules/tools/spindle/HuanyangSpindleControl.cpp
+++ src/modules/tools/spindle/HuanyangSpindleControl.cpp
@@ -5,42 +5,32 @@
       You should have received a copy of the GNU General Public License along with Smoothie. If not, see <http://www.gnu.org/licenses/>.
 */
 
-/* 
-    Hunayang RS485 communication protocol
+/*
+     Sako SKI780 RS485 communication protocol
 
-    originally found on cnczone.nl, posted by Rikkepic:
-    http://cnczone.nl/viewtopic.php?f=35&t=11605
+    Got the Sako SKI780 RS485 communication protocol form the manufacture
+    Posted it on: https://github.com/Rickishier/SKI780-485-communication-protocol-English-Edition
 
     Parameters
 
-    PD001   2   RS485 Control of run commands
-    PD002   2   RS485 Control of operating frequency
-    PD023   1   Reverse run enabled
-    PD163   1   RS485 Address: 1
-    PD164   1   RS485 Baud rate: 9600
-    PD165   3   RS485 Mode: RTU, 8N1
-
-    == Function Read == 
-
-    ADDR    CMD     LEN     PAR     DATA        CRC
-    0x01    0x01    0x03    0xA5    0x00 0x00   0x2C 0x6D       Read PD165 (165=0xA5)
-
-    == Function Write ==
-
-    ADDR    CMD     LEN     PAR     DATA        CRC
-    0x01    0x02    0x03    0x03    0x09 0xC4   0x8F 0x8D       Write PD003 (0x9C4 = 2500 = 25.00Hz)
+    P0-02   2      Command source selection
+    P0-03   9      Main frequenty source 9 = comunication settings
+    PD-00   6005   Baud rate   
+    PD-01   3      Modbus data format, 8N1
+    PD-02   1      Local address: 1
+    PD-05   31     Modbus protocol selection
 
     == Control Write ==
 
-    ADDR    CMD     LEN     DATA    CRC
-    0x01    0x03    0x01    0x01    0x31 0x88                   Start spindle clockwise
+    ADDR    CMD     LEN     		DATA		CRC
+    0x01    0x06    0x20 0x00	0x00 0x01    0x43 0xCA                   Start spindle clockwise
 
-    ADDR    CMD     LEN     DATA    CRC
-    0x01    0x03    0x01    0x08    0xF1 0x8E                   Stop spindle
+    ADDR    CMD     LEN				DATA		CRC
+    0x01    0x06    0x20 0x00	0x00 0x06    0x02 0x08                   Stop spindle
 
-    ADDR    CMD     LEN     DATA    CRC
-    0x01    0x03    0x01    0x11    0x30 0x44                   Start spindle counter-clockwise
-
+    ADDR    CMD     LEN           	DATA    	CRC
+    0x01    0x06    0x20 0x00    0x00 0x02	0x30 0x44              Start spindle counter-clockwise
+0
     == Control Read ==
 
     ADDR    CMD     LEN     PAR     DATA        CRC
@@ -76,21 +66,21 @@
 #include "gpio.h"
 #include "Modbus.h"
 
-void HuanyangSpindleControl::turn_on() 
+void HuanyangSpindleControl::turn_on()
 {
     // prepare data for the spindle off command
-    char turn_on_msg[6] = { 0x01, 0x03, 0x01, 0x01, 0x00, 0x00 };
+    char turn_on_msg[8] = { 0x01, 0x06, 0x20, 0x00, 0x00, 0x01, 0x43, 0xCA};
     // calculate CRC16 checksum
-    unsigned int crc = modbus->crc16(turn_on_msg, sizeof(turn_on_msg)-2);
-    turn_on_msg[4] = crc & 0xFF;
-    turn_on_msg[5] = (crc >> 8);
+    //unsigned int crc = modbus->crc16(turn_on_msg, sizeof(turn_on_msg)-2);
+    //turn_on_msg[6] = crc & 0xFF;
+    //turn_on_msg[7] = (crc >> 8);
 
     // enable transmitter
     modbus->dir_output->set();
     modbus->delay(1);
     // send the actual message
     modbus->serial->write(turn_on_msg, sizeof(turn_on_msg));
-    // wait a calculated time for the data to be sent 
+    // wait a calculated time for the data to be sent
     modbus->delay((int) ceil(sizeof(turn_on_msg) * modbus->delay_time));
     // disable transmitter
     modbus->dir_output->clear();
@@ -100,21 +90,22 @@ void HuanyangSpindleControl::turn_on()
 
 }
 
-void HuanyangSpindleControl::turn_off() 
+// FIXME this cannot call delay() as it calls on_idle and this can be called from on_idle so recursion
+void HuanyangSpindleControl::turn_off()
 {
     // prepare data for the spindle off command
-    char turn_off_msg[6] = { 0x01, 0x03, 0x01, 0x08, 0x00, 0x00 };
+    char turn_off_msg[8] = { 0x01, 0x06, 0x20, 0x00, 0x00, 0x06, 0x02, 0x08};
     // calculate CRC16 checksum
-    unsigned int crc = modbus->crc16(turn_off_msg, sizeof(turn_off_msg)-2);
-    turn_off_msg[4] = crc & 0xFF;
-    turn_off_msg[5] = (crc >> 8);
+    //unsigned int crc = modbus->crc16(turn_off_msg, sizeof(turn_off_msg)-2);
+    //turn_off_msg[6] = crc & 0xFF;
+    //turn_off_msg[7] = (crc >> 8);
 
     // enable transmitter
     modbus->dir_output->set();
     modbus->delay(1);
     // send the actual message
     modbus->serial->write(turn_off_msg, sizeof(turn_off_msg));
-    // wait a calculated time for the data to be sent 
+    // wait a calculated time for the data to be sent
     modbus->delay((int) ceil(sizeof(turn_off_msg) * modbus->delay_time));
     // disable transmitter
     modbus->dir_output->clear();
@@ -124,26 +115,26 @@ void HuanyangSpindleControl::turn_off()
 
 }
 
-void HuanyangSpindleControl::set_speed(int target_rpm) 
+void HuanyangSpindleControl::set_speed(int target_rpm)
 {
 
     // prepare data for the set speed command
-    char set_speed_msg[7] = { 0x01, 0x05, 0x02, 0x00, 0x00, 0x00, 0x00 };
+    char set_speed_msg[8] = { 0x01, 0x06, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00};
     // convert RPM into Hz
-    unsigned int hz = target_rpm / 60 * 100; 
-    set_speed_msg[3] = (hz >> 8);
-    set_speed_msg[4] = hz & 0xFF;
+    unsigned int hz = target_rpm / 240 * 100;
+    set_speed_msg[4] = (hz >> 8);
+    set_speed_msg[5] = hz & 0xFF;
     // calculate CRC16 checksum
     unsigned int crc = modbus->crc16(set_speed_msg, sizeof(set_speed_msg)-2);
-    set_speed_msg[5] = crc & 0xFF;
-    set_speed_msg[6] = (crc >> 8);
+    set_speed_msg[7] = (crc >> 8);
+	set_speed_msg[6] = crc & 0xFF;
 
     // enable transmitter
     modbus->dir_output->set();
     modbus->delay(1);
     // send the actual message
     modbus->serial->write(set_speed_msg, sizeof(set_speed_msg));
-    // wait a calculated time for the data to be sent 
+    // wait a calculated time for the data to be sent
     modbus->delay((int) ceil(sizeof(set_speed_msg) * modbus->delay_time));
     // disable transmitter
     modbus->dir_output->clear();
@@ -152,7 +143,7 @@ void HuanyangSpindleControl::set_speed(int target_rpm)
 
 }
 
-void HuanyangSpindleControl::report_speed() 
+void HuanyangSpindleControl::report_speed()
 {
     // clear RX buffer before start
     while(modbus->serial->readable()){
@@ -160,18 +151,18 @@ void HuanyangSpindleControl::report_speed()
     }
 
     // prepare data for the get speed command
-    char get_speed_msg[8] = { 0x01, 0x04, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00 };
-    // calculate CRC16 checksum
-    unsigned int crc = modbus->crc16(get_speed_msg, sizeof(get_speed_msg)-2);
-    get_speed_msg[6] = crc & 0xFF;
-    get_speed_msg[7] = (crc >> 8);
+    char get_speed_msg[8] = { 0x01, 0x03, 0x10, 0x07, 0x00, 0x00, 0xF0, 0xCB };
+    // calculate CRC16 checksum  CBF0
+    //unsigned int crc = modbus->crc16(get_speed_msg, sizeof(get_speed_msg)-2);
+    //get_speed_msg[6] = crc & 0xFF;
+    //get_speed_msg[7] = (crc >> 8);
 
     // enable transmitter
     modbus->dir_output->set();
     modbus->delay(1);
     // send the actual message
     modbus->serial->write(get_speed_msg, sizeof(get_speed_msg));
-    // wait a calculated time for the data to be sent 
+    // wait a calculated time for the data to be sent
     modbus->delay((int) ceil(sizeof(get_speed_msg) * modbus->delay_time));
     // disable transmitter
     modbus->dir_output->clear();
@@ -182,7 +173,7 @@ void HuanyangSpindleControl::report_speed()
     modbus->delay((int) ceil(8 * modbus->delay_time));
     // prepare an array for the answer
     char speed[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-   
+
     // read the answer into the buffer
     for(int i=0; i<8; i++) {
         speed[i] = modbus->serial->getc();
