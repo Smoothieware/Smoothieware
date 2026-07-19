9a630f097ea4718e691715d59a60dd6bb21501d2
Jim Morris <morris@wolfman.com>
2026-07-19 11:15:22 +0100

    fix G43.1 to set tool offset rather than increment it

--- src/modules/robot/Robot.cpp
+++ src/modules/robot/Robot.cpp
@@ -574,7 +574,7 @@ void Robot::on_gcode_received(void *argument)
             case 21: this->inch_mode = false;   break;
 
             case 43:
-                if(gcode->subcode == 1 || gcode->subcode == 2) {
+                if(gcode->subcode >= 1 && gcode->subcode <= 2) {
                     float deltas[3] = {0, 0, 0};
                     if(gcode->has_letter('X')) deltas[X_AXIS] = to_millimeters(gcode->get_value('X'));
                     if(gcode->has_letter('Y')) deltas[Y_AXIS] = to_millimeters(gcode->get_value('Y'));
@@ -582,15 +582,20 @@ void Robot::on_gcode_received(void *argument)
 
                     float x, y, z;
                     std::tie(x, y, z) = tool_offset;
-                    if(deltas[X_AXIS] != 0) x += deltas[X_AXIS];
-                    if(deltas[Y_AXIS] != 0) y += deltas[Y_AXIS];
-                    if(deltas[Z_AXIS] != 0) z += deltas[Z_AXIS];
-                    tool_offset = wcs_t(x, y, z);
-
                     if(gcode->subcode == 2) {
-                        // we also move
+                        // incremental
+                        if(deltas[X_AXIS] != 0) x += deltas[X_AXIS];
+                        if(deltas[Y_AXIS] != 0) y += deltas[Y_AXIS];
+                        if(deltas[Z_AXIS] != 0) z += deltas[Z_AXIS];
+                        // we also move, which is NOT in the spec, but we use this for babysteps
                         delta_move(deltas, this->seek_rate / seconds_per_minute, 3);
-                    }
+                    } else {
+                        // as per linuxcnc spec, sets the tool offset
+                        if(gcode->has_letter('X')) x = deltas[X_AXIS];
+                        if(gcode->has_letter('Y')) y = deltas[Y_AXIS];
+                        if(gcode->has_letter('Z')) z = deltas[Z_AXIS];
+                     }
+                    tool_offset = wcs_t(x, y, z);
                 }
                 break;
 
